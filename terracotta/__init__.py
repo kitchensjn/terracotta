ed105a2e4c84c0c8b5fa62882afcee8fc778b6d9
James Kitchens <40303683+kitchensjn@users.noreply.github.com>
2024-11-13 10:36:06 -0800

    #4 - grid world map file generator
    Pretty straightforward, and pairs with grid.slim so that we can run simulations at different sizes.

--- terracotta/__init__.py
+++ terracotta/__init__.py
@@ -40,3 +40,30 @@ def calc_tree_log_likelihood(tree, sample_locs, transition_matrix):
             outgoing_log_message = np.array([logsumexp(np.log(linalg.expm(transition_matrix*tree.branch_length(child))).T + summed_log_messages, axis=1)])
             log_messages[node] = outgoing_log_message
     return logsumexp(outgoing_log_message), outgoing_log_message
+
+def create_grid_world_map_file(side_length, path="world_map.tsv"):
+    """Creates the world map input file that pairs with grid.slim of specified size
+
+    Parameters
+    ----------
+    side_length : int
+        Number of populations along one side of the square grid metapopulation (`metapopSide` in grid.slim)
+    path : str
+        Path to output file. (default="world_map.tsv")
+    """
+
+    with open(path, "w") as outfile:
+        outfile.write("id\txcoord\tycoord\tneighbours\n")
+        for y in range(side_length):
+            for x in range(side_length):
+                neighbors = []
+                if (y > 0):
+                    neighbors.append(str(x+(y-1)*side_length))
+                if (x > 0):
+                    neighbors.append(str((x-1)+y*side_length))
+                if (x < side_length-1):
+                    neighbors.append(str((x+1)+y*side_length))
+                if (y < side_length-1):
+                    neighbors.append(str(x+(y+1)*side_length))
+                outfile.write(f"{x+y*side_length}\t{x}\t{y}\t{",".join(neighbors)}\n")
+            
