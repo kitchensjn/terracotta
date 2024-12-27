6c74161ebe27e38b1854958b134338645538c8ff
James Kitchens <40303683+kitchensjn@users.noreply.github.com>
2024-12-27 16:49:48 -0500

    deme types and devlog
    I've started a devlogs that are on a two week interval. This seems to give be enough time to develop new features and also document things. Even still, there have been some changes between even the first two that must be documented. Demes now have a type which affects the migration rates. This allows testing of whether `terracotta` can estimate multiple rates at the same time.

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # terracotta
+
 A belief propagation method for estimating the location of genetic ancestors from gene trees.
+
+
+## Creating environment
+
+Download the `.zip` of the repository from GitHub. This tutorial uses `conda` to manage the environment,
+though there are many options if you prefer another.
+
+```
+conda env create -f environment.yml
+conda activate terracotta
+```
+
+## Importing terracotta
+
+```
+import terracotta as tct
+```
+
+## Input Dataset
+
+There are three main inputs to 
+
+### `trees/`
+
+A directory of `.trees` files in the `tskit` format. These files only contain a single tree each. In the future, this format may be
+changed to a file of newick trees, but for now, many of the `tskit` functions are being used.
+
+### `samples.tsv`
+
+
+
+### `world_map.tsv`
+
+### Simulating a dataset for a grid demography with `msprime`
+
+![INSERT A PICTURE OF THE GRID]
+
+`terrracotta` provides functionality for simulating a dataset under a grid demography using `msprime`. This has been useful for testing
+the accuracy of migration rate estimates.
+
+```
+tct.create_grid_demography_dataset(
+    side_length=10,
+    number_of_deme_types=2,
+    number_of_samples=25,
+    allow_multiple_samples_per_deme=False,
+    number_of_trees=100,
+    ploidy=1,
+    pop_size=500,
+    migration_rates={0:0.01, 1:0.00001, 2:0.01},
+    output_directory=directory
+)
+```
--- terracotta/__init__.py
+++ terracotta/__init__.py
@@ -1,12 +1,451 @@
 import numpy as np
 from scipy import linalg
 from scipy.special import logsumexp
+import tskit
+import networkx as nx
+import msprime
+from os import mkdir
+import random
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+class WorldMap:
+    """Stores a map of the demes
+
+    Attributes
+    ----------
+    demes : pandas.DataFrame
+    connections : pandas.DataFrame
+    """
+
+    def __init__(self, demes):
+        """Initializes the WorldMap object
+
+        Parameters
+        ----------
+        demes : pandas.DataFrame
+            Five columns: "id", "xcoord", "ycoord", "type", "neighbours" (note the "u" in neighbours).
+            See README.md for more details about `demes.tsv` file structure
+        """
+
+        self.demes = demes
+        deme_types = self.demes["type"].unique()
+        connection_types = []
+        for i,type0 in enumerate(deme_types):
+            for type1 in deme_types[i:]:
+                if type1 > type0:
+                    connection_types.append((type0, type1))
+                else:
+                    connection_types.append((type1, type0))
+        connections = []
+        converted_neighbors = []
+        for index,row in demes.iterrows():
+            neighbors = row["neighbours"].split(",")
+            int_neighbors = []
+            for neighbor in neighbors:
+                neighbor = int(neighbor)
+                int_neighbors.append(neighbor)
+                if row["id"] < neighbor:
+                    neighbor_type = self.get_deme_type(neighbor)
+                    if neighbor_type > row["type"]:
+                        ct = connection_types.index((row["type"], neighbor_type))
+                    else:
+                        ct = connection_types.index((neighbor_type, row["type"]))
+                    connections.append({"deme_0":row["id"], "deme_1":neighbor, "type":ct})
+            converted_neighbors.append(int_neighbors)
+        self.demes["neighbours"] = converted_neighbors
+        self.connections = pd.DataFrame(connections)
+
+    def get_coordinates_of_deme(self, id):
+        """Gets x and y coordinates for specified deme
+
+        Parameters
+        ----------
+        id : int
+            ID of deme
+
+        Returns
+        -------
+        coords : tuple
+            (x, y) where x and y are both ints or floats
+        """
+
+        row = self.demes.loc[self.demes["id"]==id,:].iloc[0]
+        coords = (row["xcoord"], row["ycoord"])
+        return coords
+    
+    def get_deme_type(self, id):
+        """Gets the type for specified deme
+
+        Wrapper of pandas.DataFrame function. Assumes that deme IDs are unique.
+
+        Parameters
+        ----------
+        id : int
+            ID of deme
+
+        Returns
+        -------
+        deme_type : int
+            Type of deme
+        """
+
+        deme_type = self.demes.loc[self.demes["id"]==id,"type"].iloc[0]
+        return deme_type
+
+    def get_neighbors_of_deme(self, id):
+        """Gets the neighbors of a deme
+
+        Wrapper of pandas.DataFrame function. Assumes that deme IDs are unique.
+
+        Parameters
+        ----------
+        id : int
+            ID of deme
+
+        Returns
+        -------
+        neighbors : pd.Series
+            Contains all of the neighbors of specified deme
+        """
+
+        neighbors = self.demes.loc[self.demes["id"]==id,"neighbours"].iloc[0]
+        return neighbors
+    
+    get_neighbours_of_deme = get_neighbors_of_deme
+
+    def draw(self, color_demes=False, color_connections=False):
+        """Draws the world map
+
+        Uses matplotlib.pyplot
+
+        Parameters
+        ----------
+        color_demes : bool
+            Whether to color the demes based on type
+        color_connections : bool
+            Whether to color the connections based on type
+        """
+
+        if color_connections:
+            num_connection_types = len(self.connections["type"].unique())
+            color_rnd = random.Random()
+            color_rnd.seed(1)
+            connection_colors = ["#"+''.join([color_rnd.choice("0123456789ABCDEF") for j in range(6)]) for i in range(num_connection_types)]
+        for index,row in self.connections.iterrows():
+            deme_0 = self.get_coordinates_of_deme(row["deme_0"])
+            deme_1 = self.get_coordinates_of_deme(row["deme_1"])
+            if color_connections:
+                plt.plot([deme_0[0], deme_1[0]], [deme_0[1], deme_1[1]], color=connection_colors[row["type"]], linewidth=5)
+            else:
+                plt.plot([deme_0[0], deme_1[0]], [deme_0[1], deme_1[1]], color="grey")
+        if color_demes:
+            plt.scatter(self.demes["xcoord"], self.demes["ycoord"], c=self.demes["type"], zorder=2, s=100)
+        else:
+            plt.scatter(self.demes["xcoord"], self.demes["ycoord"], zorder=2, color="grey")
+        plt.axis("off")
+        plt.show()
+
+    def build_transition_matrix(self, migration_rates):
+        """Builds the transition matrix based on the world map and migration rates
+
+        Parameters
+        ----------
+        migration_rates : dict
+            Keys are the connection type and values are the instantaneous migration
+            rate along that connection
+        
+        Returns
+        -------
+        transition_matrix : np.array
+        """
+
+        transition_matrix = np.zeros((len(self.demes),len(self.demes)))
+        for index,connection in self.connections.iterrows():
+            i_0 = self.demes.loc[self.demes["id"]==connection["deme_0"]].index[0]
+            i_1 = self.demes.loc[self.demes["id"]==connection["deme_1"]].index[0]
+            rate = migration_rates.get(connection["type"], -1)
+            if rate == -1:
+                raise RuntimeError(f"Rate for connection of type '{connection["type"]}' is not provided. Please specify and try again.")
+            transition_matrix[i_0, i_1] = rate
+            transition_matrix[i_1, i_0] = rate
+        diag = -np.sum(transition_matrix, axis=1)
+        np.fill_diagonal(transition_matrix, diag)
+        return transition_matrix
+
+    def build_sample_location_vectors(self, sample_locations):
+        """Builds sample location vectors based on the ordering of states in the world map
+
+        Parameters
+        ----------
+        sample_locations : dict
+
+        Returns
+        -------
+        sample_location_vectors : dict
+        """
+
+        zeros = np.zeros((1,len(self.demes)))
+        sample_location_vectors = {}
+        for index,row in sample_locations.iterrows():
+            sample_loc = zeros.copy()
+            sample_loc[0,self.demes.loc[self.demes["id"]==row["deme"]].index[0]] = 1
+            sample_location_vectors[row["id"]] = sample_loc
+        return sample_location_vectors
+
+
+
+
+
+
+
+
+
+
+
+
+
+def _create_grid_demes_file(side_length, number_of_deme_types=1, output_path="demes.tsv"):
+    """Creates the world map input file that pairs with grid.slim or msprime of specified size
+
+    Parameters
+    ----------
+    side_length : int
+        Number of populations along one side of the square grid metapopulation
+    number_of_deme_types : int
+        Set the number of deme types that will be randomly assigned. (default=1)
+    path : str
+        Path to output file. (default="demes.tsv")
+    """
+
+    with open(output_path, "w") as outfile:
+        outfile.write("id\txcoord\tycoord\ttype\tneighbours\n")
+        for y in range(side_length):
+            for x in range(side_length):
+                neighbors = []
+                if (y > 0):
+                    neighbors.append(str(x+(y-1)*side_length))
+                if (x > 0):
+                    neighbors.append(str((x-1)+y*side_length))
+                if (x < side_length-1):
+                    neighbors.append(str((x+1)+y*side_length))
+                if (y < side_length-1):
+                    neighbors.append(str(x+(y+1)*side_length))
+                outfile.write(f"{x+y*side_length}\t{x}\t{y}\t{random.randint(0,number_of_deme_types-1)}\t{",".join(neighbors)}\n")
+
+def _set_up_msprime_demography(world_map, pop_size, migration_rates):
+    """Creates the msprime.Demography object for simulating trees
+
+    Parameters
+    ----------
+    world_map : terracotta.WorldMap
+    pop_size : int
+        The population size of each deme
+    migration_rates : dict
+        Key is the type of connection and value is the migration rate of that connection type
+    """
+
+    demography = msprime.Demography()
+    for id in world_map.demes["id"]:
+        demography.add_population(name="Pop_"+str(id), initial_size=pop_size)
+    for index,connection in world_map.connections.iterrows():
+        rate = migration_rates.get(connection["type"], -1)
+        if rate == -1:
+            raise RuntimeError(f"Rate for connection of type '{connection["type"]}' is not provided. Please specify and try again.")
+        demography.set_migration_rate("Pop_"+str(connection["deme_0"]), "Pop_"+str(connection["deme_1"]), rate)
+        demography.set_migration_rate("Pop_"+str(connection["deme_1"]), "Pop_"+str(connection["deme_0"]), rate)
+    return demography
+
+def _simulate_independent_trees(
+        world_map,
+        number_of_trees,
+        number_of_samples,
+        ploidy,
+        allow_multiple_samples_per_deme,
+        pop_size,
+        migration_rates
+    ):
+    """Simulates trees under a demographic model set by the world map
+
+    Parameters
+    ----------
+    world_map : terracotta.WorldMap
+    number_of_trees : int
+        The number of independent trees to simulate
+    number_of_samples : int
+    ploidy : int
+        The ploidy of the samples
+    allow_multiple_samples_per_deme : bool
+    pop_size : int
+        The population size of each deme
+    migration_rates : dict
+        Key is the type of connection and value is the migration rate of that connection type
+    """
+
+    demography = _set_up_msprime_demography(world_map=world_map, pop_size=pop_size, migration_rates=migration_rates)
+    random_samples = np.random.choice(world_map.demes["id"], number_of_samples, replace=allow_multiple_samples_per_deme)
+    samples = {}
+    for s in random_samples:
+        samples["Pop_"+str(s)] = samples.get("Pop_"+str(s), 0) + 1
+    for i in range(number_of_trees):
+        ts = msprime.sim_ancestry(
+            samples=samples,
+            ploidy=ploidy,
+            demography=demography
+        )
+        yield ts
+
+def _create_samples_file_from_tree_sequence(input_trees_path=None, ts=None, output_samples_path="samples.tsv", num_samples=-1, output_trees_path="samples.trees", random_seed=None):
+    """Creates the samples input file from a tree sequence.
+
+    Optionally, subsets tree sequence to a specified number of random samples.
+
+    Parameters
+    ----------
+    input_trees_path : str
+    ts : tskit:TreeSequence
+    output_samples_path : str
+    num_samples : int
+    output_trees_path : str
+    random_seed : int
+    """
+
+    if input_trees_path is not None:
+        ts = tskit.load(input_trees_path)
+    elif ts is not None:
+        pass
+    else:
+        raise RuntimeError("Must provide either an `input_trees_path` or `ts` as input.")
+
+    if random_seed != None:
+        np.seed(random_seed)
+
+    if num_samples > 1:
+        s = np.random.choice(ts.num_samples, num_samples, replace=False)
+        ts = ts.simplify(samples=s)
+        ts.dump(output_trees_path)
+
+    with open(output_samples_path, "w") as outfile:
+        outfile.write("id\tstate\n")
+        for sample in ts.samples():
+            outfile.write(f"{sample}\t{ts.node(sample).population}\n")
+
+def _create_samples_and_trees_files(
+        demes_path,
+        number_of_trees,
+        number_of_samples,
+        ploidy,
+        allow_multiple_samples_per_deme,
+        pop_size,
+        migration_rates,
+        output_directory="."
+    ):
+    """
+    Parameters
+    ----------
+    demes_path : string
+    number_of_trees : int
+        The number of independent trees to simulate
+    number_of_samples : int
+    ploidy : int
+        The ploidy of the samples
+    allow_multiple_samples_per_deme : bool
+    pop_size : int
+        The population size of each deme
+    migration_rates : dict
+        Key is the type of connection and value is the migration rate of that connection type
+    output_directory : string
+        (default=".")
+    """
+
+    demes = pd.read_csv(demes_path, sep="\t")
+    if (not allow_multiple_samples_per_deme) and (number_of_samples > len(demes)):
+        raise RuntimeError(f"There are more samples than demes ({number_of_samples} versus {len(demes)}). You must either allow multiple samples per deme or reduce the number of samples.")
+    world_map = WorldMap(demes=demes)
+    mkdir(f"{output_directory}/trees")
+    trees = _simulate_independent_trees(
+        world_map=world_map,
+        number_of_trees=number_of_trees,
+        number_of_samples=number_of_samples,
+        ploidy=ploidy,
+        allow_multiple_samples_per_deme=allow_multiple_samples_per_deme,
+        pop_size=pop_size,
+        migration_rates=migration_rates
+    )           
+    for i,tree in enumerate(trees):
+        tree.dump(f"{output_directory}/trees/{i}.trees")
+    _create_samples_file_from_tree_sequence(
+        ts=tree,
+        output_samples_path=f"{output_directory}/samples.tsv"
+    )
+
+def create_grid_demography_dataset(
+        side_length,
+        number_of_deme_types,
+        number_of_trees,
+        number_of_samples,
+        ploidy,
+        allow_multiple_samples_per_deme,
+        pop_size,
+        migration_rates,
+        output_directory
+    ):
+    """Creates a new dataset based on the specified grid metapopulation demographic model
+
+    Creates a directory at "output_directory" with a "demes.tsv", "samples.tsv", ".trees" files
+
+    TODO: Add a check for whether a directory exists already
+
+    Parameters
+    ----------
+    side_length : int
+        Number of demes along one side length of the grid
+    number_of_deme_types : int
+        Set the number of deme types that will be randomly assigned
+    number_of_trees : int
+        Number of trees to simulate from the demographic model
+    number_of_samples : int
+        Number of samples to place in the demes
+    ploidy : int
+        The ploidy (number of chromosomes) of those samples
+    allow_multiple_samples_per_deme : bool
+        Whether to allow randomly placing multiple samples into a deme
+    pop_size : int
+        Population size of each deme
+    migration_rates : dict
+        Key is the type of connection and value is the migration rate of that connection type
+    output_directory : string
+        Name of the directory that will be created to hold the output files
+    """
+
+    mkdir(output_directory)
+    _create_grid_demes_file(
+        side_length=side_length,
+        number_of_deme_types=number_of_deme_types,
+        output_path=f"{output_directory}/demes.tsv"
+    )
+    _create_samples_and_trees_files(
+        demes_path=f"{output_directory}/demes.tsv",
+        number_of_samples=number_of_samples,
+        allow_multiple_samples_per_deme=allow_multiple_samples_per_deme,
+        number_of_trees=number_of_trees,
+        ploidy=ploidy,
+        pop_size=pop_size,
+        migration_rates=migration_rates,
+        output_directory=f"{output_directory}"
+    )
+
+
+
+
+
+
 
 
 def calc_tree_log_likelihood(tree, sample_locs, transition_matrix):
     """Calculates the log_likelihood of the tree using Felsenstein's Pruning Algorithm.
 
-    NOTE: This only works for trees with a single root.
     NOTE: Assumes that samples are always tips on the tree.
 
     Parameters
@@ -21,9 +460,9 @@ def calc_tree_log_likelihood(tree, sample_locs, transition_matrix):
     Returns
     -------
     tree_likelihood : float
-        likelihood of the tree (sum of the root locations likelihood vector)
-    locs[node] : np.array
-        root locations likelihood vector
+        likelihood of the tree (product of the root likelihoods)
+    root_log_likes : list
+        List of root likelihoods (sum of the root locations vector)
     """
 
     log_messages = {}
@@ -39,31 +478,9 @@ def calc_tree_log_likelihood(tree, sample_locs, transition_matrix):
             summed_log_messages = np.sum(incoming_log_messages, axis=0)
             outgoing_log_message = np.array([logsumexp(np.log(linalg.expm(transition_matrix*tree.branch_length(child))).T + summed_log_messages, axis=1)])
             log_messages[node] = outgoing_log_message
-    return logsumexp(outgoing_log_message), outgoing_log_message
+    
+    roots = tree.roots
+    root_log_likes = [logsumexp(log_messages[r]) for r in roots]
+    tree_likelihood = sum(root_log_likes)
 
-def create_grid_world_map_file(side_length, path="world_map.tsv"):
-    """Creates the world map input file that pairs with grid.slim of specified size
-
-    Parameters
-    ----------
-    side_length : int
-        Number of populations along one side of the square grid metapopulation (`metapopSide` in grid.slim)
-    path : str
-        Path to output file. (default="world_map.tsv")
-    """
-
-    with open(path, "w") as outfile:
-        outfile.write("id\txcoord\tycoord\tneighbours\n")
-        for y in range(side_length):
-            for x in range(side_length):
-                neighbors = []
-                if (y > 0):
-                    neighbors.append(str(x+(y-1)*side_length))
-                if (x > 0):
-                    neighbors.append(str((x-1)+y*side_length))
-                if (x < side_length-1):
-                    neighbors.append(str((x+1)+y*side_length))
-                if (y < side_length-1):
-                    neighbors.append(str(x+(y+1)*side_length))
-                outfile.write(f"{x+y*side_length}\t{x}\t{y}\t{",".join(neighbors)}\n")
-            
+    return tree_likelihood, root_log_likes
