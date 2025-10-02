7aa5506103f6220e09b4cc9095e5005ab6e75d41
James Kitchens <40303683+kitchensjn@users.noreply.github.com>
2025-10-02 14:49:19 -0700

    epochs belief propagation implementation - currently broken

---
+++ devlog/20250929/assets/epochs/view.py
@@ -0,0 +1,8 @@
+import tskit
+from glob import glob
+
+for ts in glob(f"dataset/sim/trees/*"):
+    ts = tskit.load(ts).simplify()
+    print(ts.tables.nodes.time[-3:])
+    print(ts.draw_text())
+    exit()
