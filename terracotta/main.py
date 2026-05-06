099d790f2b4efc87cf1902dc981104404715bd77
James Kitchens <40303683+kitchensjn@users.noreply.github.com>
2026-05-06 14:20:56 -0700

    tracking lineage over time
    Feel like it's getting close to a release, now that seemingly all of the functionality is there. Not prioritizing optimizing the lineage tracking for the time being as it won't be run as often as the migration surface estimator.

--- terracotta/main.py
+++ terracotta/main.py
@@ -1009,3 +1009,130 @@ def estimate_node_positions(
             combined = np.prod(np.concatenate((messages[[id+len(parents)]], messages[np.where(parents==id)[0]])), axis=0)
             node_locations[id] = combined / sum(combined)
     return node_locations
+
+def ancs(tree, u):
+    """Find all of the ancestors above a node for a tree
+
+    Taken directly from https://github.com/tskit-dev/tskit/issues/2706
+
+    Parameters
+    ----------
+    tree : tskit.Tree
+    u : int
+        The ID for the node of interest
+
+    Returns
+    -------
+    An iterator over the ancestors of u in this tree
+    """
+
+    u = tree.parent(u)
+    while u != -1:
+         yield u
+         u = tree.parent(u)
+
+def track_lineage_over_time(
+        sample,
+        times,
+        tree,
+        world_map,
+        parameters
+    ):
+    """Marginal scaled likelihoods of node positions
+    """
+
+    ancestors = [sample] + list(ancs(tree=tree, u=sample))
+
+    node_times = []
+    for a in ancestors:
+        node_times.append(int(tree.time(a)))
+
+    pc_combos = []
+    for t in times:
+        for i,v in enumerate(node_times):
+            if v > t:
+                child = ancestors[i-1]
+                parent = ancestors[i]
+                break
+            elif v == t:
+                child = ancestors[i]
+                parent = ancestors[i]
+                break
+        pc_combos.append((child, parent))
+
+    sample_locations_array, sample_ids = world_map.build_sample_locations_array()
+
+    parents, branch_above, time_bin_widths, ids_asc_time = _deconstruct_tree(tree, world_map.epochs)
+    transition_matrices = world_map.build_transition_matrices(parameters=parameters)
+
+    messages = _calc_all_messages(
+        parents,
+        branch_above,
+        ids_asc_time,
+        sample_locations_array,
+        sample_ids,
+        transition_matrices
+    )
+
+    positions = np.zeros((len(pc_combos), len(world_map.demes)))
+    for element, node_combo in enumerate(pc_combos):
+        if node_combo[0] == node_combo[1]:
+            if node_combo[0] in sample_ids:
+                node_pos = sample_locations_array[np.where(sample_ids==node_combo[0])[0][0]]
+            else:
+                combined = np.prod(np.concatenate((messages[[node_combo[0]+len(parents)]], messages[np.where(parents==node_combo[0])[0]])), axis=0)
+                node_pos = combined / sum(combined)
+        else:
+            if node_combo[0] in sample_ids:
+                child_pos = sample_locations_array[np.where(sample_ids==node_combo[0])[0][0]]
+            else:
+                incoming_child_messages = messages[np.where(parents==node_combo[0])[0]]
+                if len(incoming_child_messages) > 0:
+                    combined = np.prod(incoming_child_messages, axis=0)
+                    child_pos = combined / sum(combined)
+                else:
+                    child_pos = np.ones((1,len(world_map.demes)))[0]
+            if node_combo[1] in sample_ids:
+                parent_pos = sample_locations_array[np.where(sample_ids==node_combo[1])[0][0]]
+            else:
+                backward_messages = np.where(parents==node_combo[1])[0]
+                backward_messages = backward_messages[backward_messages != node_combo[0]]
+                incoming_parent_messages = np.concatenate((messages[[node_combo[1]+len(parents)]], messages[backward_messages]))
+                if len(incoming_parent_messages) > 0:
+                    combined = np.prod(incoming_parent_messages, axis=0)
+                    parent_pos = combined / sum(combined)
+                else:
+                    parent_pos = np.ones((1,len(world_map.demes)))[0]
+            
+            branch_length_to_child = int(times[element] - tree.time(node_combo[0]))
+            bl_child = branch_above[:, node_combo[0]].copy()
+            bl_parent = bl_child.copy()
+            whats_left = branch_length_to_child
+            for e in range(len(world_map.epochs)):
+                current = bl_parent[e].copy()
+                if current >= whats_left:
+                    bl_parent[e] -= whats_left
+                else:
+                    bl_parent[e] = 0
+                whats_left -= current
+            bl_child -= bl_parent
+
+            outgoing_child_message = _calc_branch_message(child_pos, bl_child, transition_matrices, direction="backward")
+            outgoing_parent_message = _calc_branch_message(parent_pos, bl_parent, transition_matrices, direction="forward")
+            node_pos = np.multiply(outgoing_child_message, outgoing_parent_message)
+            node_pos = node_pos / np.sum(node_pos)
+        positions[element] = node_pos
+    
+    return positions
+
+
+
+
+    node_locations = np.zeros((len(ids_asc_time), len(sample_locations_array[0])))
+    for id in ids_asc_time:
+        if id in sample_ids:
+            node_locations[id] = sample_locations_array[np.where(sample_ids==id)[0][0]]
+        else:
+            combined = np.prod(np.concatenate((messages[[id+len(parents)]], messages[np.where(parents==id)[0]])), axis=0)
+            node_locations[id] = combined / sum(combined)
+    return node_locations
